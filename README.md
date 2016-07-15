b212210144811b5d34c1b2360aff8cc9ef24198c
thomashare <tommyhares@gmail.com>
2016-07-14 19:00:12 -0700

    Added some more automated things to the gulpfile as well as updated the README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Majesti
-A beautiful web development framework.
+[logo]: https://camo.githubusercontent.com/ee2eeb9e563705d6cd4966748fafdf34ce8143a6/68747470733a2f2f7333322e706f7374696d672e6f72672f77696b7169727439312f6d616a657374695f6c6f676f2e706e67
+
+#![Majesti logo][logo] Majesti
+__A beautiful web development framework.__  
+Majesti was created to provide a starter template for web developers that uses the most legible preprocessors available - Streamlined with the help of [Gulp](http://gulpjs.com) and [Browsersync](https://www.browsersync.io).
+
+Majesti uses [Stylus](http://stylus-lang.com), [Pug](http://jade-lang.com), [CSS-Nano](http://cssnano.co), [Autoprefixer](https://github.com/postcss/autoprefixer) and [Svg2png]() to speed up your workflow and get things done.
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
 var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var svgo = require('gulp-svgo');
 var svg2png = require('gulp-svg2png');
 
 // Static Server + watching styl/jade files
 gulp.task('serve', ['stylus'], function() {
 
   browserSync.init({
-    proxy: 'localhost:3000/name'
+    proxy: '/Users/tommy/webapps/Majesti/index.html'
   });
 
   gulp.watch("static/styl/**/*.styl", ['stylus']);
-  gulp.watch("static/styl/**/*.styl", ['jade']);
+  gulp.watch("static/styl/**/*.styl", ['pug']);
   gulp.watch("static/svg/**/*.svg", ['svg2png']);
   
 });
@@ -20,27 +23,30 @@ gulp.task('serve', ['stylus'], function() {
 // Convert pug files to html
 gulp.task("pug", function() {
   
-  return gulp.src("./templates/jade/**/*.pug")
+  return gulp.src("./templates/jade/*/*.pug")
     .pipe(pug())
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
   
 });
 
-// Compile styl into CSS & auto-inject into browsers
+// Compile styl into CSS
 gulp.task('stylus', function() {
   
   return gulp.src("static/styl/main.styl")
     .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
     .pipe(gulp.dest("static/css"))
     .pipe(browserSync.stream());
   
 });
 
-// Convert svg files to png
+// Optimize svg files and convert them to png
 gulp.task("svg2png", function() {
   
   return gulp.src("./static/svg/**/*.svg")
+    .pipe(svgo())
     .pipe(svg2png())
     .pipe(gulp.dest('./static/img'))
     .pipe(browserSync.stream());
